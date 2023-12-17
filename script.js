b04fc79d9516867a9632aaed8f15d2de7f808438
chunsig <154062251+chunsig@users.noreply.github.com>
2023-12-17 22:12:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,35 +18,10 @@ function calculate() {
     }
 
     var multipliedResult = baseValue * amount;
+    var resultString = '받을 돈: ' + formatCurrency(multipliedResult);
+
     var dividedResult = multipliedResult / 2;
+    resultString += ', 공동계좌에 넣을 돈: ' + formatCurrency(dividedResult);
 
-    document.getElementById('multipliedResult').innerText = formatCurrency(multipliedResult);
-    document.getElementById('dividedResult').innerText = formatCurrency(dividedResult);
+    document.getElementById('result').innerText = '결과: ' + resultString;
 }
-
-function copyToClipboard(elementId) {
-    var resultText = document.getElementById(elementId).innerText;
-    var textarea = document.createElement('textarea');
-    textarea.value = resultText;
-    document.body.appendChild(textarea);
-    textarea.select();
-    
-function copyToClipboard(elementId) {
-    var resultText = document.getElementById(elementId).innerText;
-    var cleanedResultText = resultText.replace('₩', '').trim(); // '₩' 기호를 제거합니다.
-    var textarea = document.createElement('textarea');
-    textarea.value = cleanedResultText;
-    document.body.appendChild(textarea);
-    textarea.select();
-
-    try {
-        var successful = document.execCommand('copy');
-        var msg = successful ? '성공적으로 복사되었습니다.' : '복사에 실패하였습니다.';
-        alert(msg);
-    } catch (err) {
-        alert('이 브라우저는 클립보드 작업을 지원하지 않습니다.');
-    }
-
-    document.body.removeChild(textarea);
-}
-
