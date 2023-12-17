7bfc1a6484595ce7b073c7508fc6f26800be563a
chunsig <154062251+chunsig@users.noreply.github.com>
2023-12-17 23:51:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,37 +26,3 @@ function calculate() {
     document.getElementById('result').innerText = '결과: ' + resultString;
 }
 
-body {
-    font-family: Arial, sans-serif;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    margin: 0;
-}
-
-.calculator {
-    text-align: center;
-}
-
-label, input, select, button {
-    margin: 5px;
-    padding: 8px;
-    font-size: 16px;
-}
-
-#result {
-    margin-top: 10px;
-}
-
-/* 아이콘 스타일링 */
-button {
-    background: url('D:\계산기icon.jpg') no-repeat center center;
-    background-size: 3000px; /* 아이콘의 크기에 맞게 조절하세요 */
-    padding-left: 3000px; /* 아이콘의 크기에 맞게 조절하세요 */
-}
-
-/* 아이콘이 눌렸을 때의 스타일링 */
-button:active {
-    background-color: #ddd; /* 클릭 시 회색으로 변경 */
-}
